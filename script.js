45d8e79c0b1d78f637c71379ab69b2c38b647b76
mayragm2 <mayracueto@gm2dev.com>
2026-06-23 11:05:51 -0300

    feat(all):add first files and class 1 exercises

---
+++ script.js
@@ -0,0 +1,25 @@
+const dias = 311;
+console.log("Hola!");
+console.log("faltan", dias, "para mi cumpleaños");
+console.log("mi cumple fue hace", 365-dias, "días");
+
+console.log("Predicción: 15")
+console.log(5 * 3);
+console.log("Predicción: 555")
+console.log("5" * 3); //en este caso se interpreta el 5 como número y no como string, y si fuera un string el resultado de la operacion serìa NaN
+console.log("Predicción:53")
+console.log("5" + 3);
+console.log("Predicción: 1")
+console.log(7 % 2);
+console.log("Predicción: Golazo")
+console.log("Gol" + "azo");
+console.log("Predicción: NaN") 
+console.log("3" - 1);  //en este caso se interpreta el 3 como número y no como string
+
+console.table([
+    {equipo: "Francia", mundiales_ganados: 2, instancia_maxima_mundial_2022: "Final"},
+    {equipo: "Brasil", mundiales_ganados: 5, instancia_maxima_mundial_2022: "Cuartos de final"},
+    {equipo: "Argentina", mundiales_ganados: 3, instancia_maxima_mundial_2022: "Final"},
+    {equipo: "Inglaterra", mundiales_ganados: 1, instancia_maxima_mundial_2022: "Cuartos de final"}
+]);
+
