1f506e249484b0596b37ec66f9603488e10c902b
mayragm2 <mayracueto@gm2dev.com>
2026-06-25 12:30:16 -0300

    feat(activity 1): show object info using . and [

--- Class 7/Actividad 1 - Ficha de jugador.js
+++ Class 7/Actividad 1 - Ficha de jugador.js
@@ -1,9 +1,12 @@
-// const jugador = {
-//     nombre:"Messi",
-//     pais: "Argentina",
-//     goles: 9,
-//     club: "Inter Miami"
+const jugador = {
+    nombre:"Messi",
+    pais: "Argentina",
+    goles: 8,
+    club: "Inter Miami"
+}
 
-// }
+jugador.goles = 9;
+console.log(jugador.nombre);
+console.log(jugador["pais"]);
 
-// console.log(jugador);
+console.log(jugador);
