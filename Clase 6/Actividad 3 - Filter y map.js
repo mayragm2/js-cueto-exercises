67ba451855177bddace6648cb32f9aae6ab04e34
mayragm2 <mayracueto@gm2dev.com>
2026-06-25 11:30:58 -0300

    feat(activity 4):rank players by goals and show their names and goals using chaining

--- Clase 6/Actividad 3 - Filter y map.js
+++ Clase 6/Actividad 3 - Filter y map.js
@@ -1,13 +1,13 @@
-const jugadores = [
-  { nombre: "Messi",       goles: 8,  pais: "Argentina"  },
-  { nombre: "Mbappé",      goles: 5,  pais: "Francia"    },
-  { nombre: "Haaland",     goles: 12, pais: "Noruega"    },
-  { nombre: "Benzema",     goles: 0,  pais: "Francia"    },
-  { nombre: "Lewandowski", goles: 3,  pais: "Polonia"    },
-  { nombre: "Kane",        goles: 2,  pais: "Inglaterra" },
-  { nombre: "Neymar",      goles: 1,  pais: "Brasil"     },
-  { nombre: "Müller",      goles: 0,  pais: "Alemania"   },
-];
+// const jugadores = [
+//   { nombre: "Messi",       goles: 8,  pais: "Argentina"  },
+//   { nombre: "Mbappé",      goles: 5,  pais: "Francia"    },
+//   { nombre: "Haaland",     goles: 12, pais: "Noruega"    },
+//   { nombre: "Benzema",     goles: 0,  pais: "Francia"    },
+//   { nombre: "Lewandowski", goles: 3,  pais: "Polonia"    },
+//   { nombre: "Kane",        goles: 2,  pais: "Inglaterra" },
+//   { nombre: "Neymar",      goles: 1,  pais: "Brasil"     },
+//   { nombre: "Müller",      goles: 0,  pais: "Alemania"   },
+// ];
 // //Parte A
 // const jugadores2goles = jugadores.filter (j => j.goles > 2);
 // console.log (jugadores2goles.length); //print out the amount of players that have more than 2 goals
