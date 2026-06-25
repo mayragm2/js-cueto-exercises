b6df347a4b3d8a9d8eccf1455ac880a3935f2ad7
mayragm2 <mayracueto@gm2dev.com>
2026-06-25 12:18:49 -0300

    feat(activity 1): comment all lines

--- Class 7/Actividad 1 - Ficha de jugador.js
+++ Class 7/Actividad 1 - Ficha de jugador.js
@@ -1,9 +1,9 @@
-const jugador = {
-    nombre:"Messi",
-    pais: "Argentina",
-    goles: 9,
-    club: "Inter Miami"
+// const jugador = {
+//     nombre:"Messi",
+//     pais: "Argentina",
+//     goles: 9,
+//     club: "Inter Miami"
 
-}
+// }
 
-console.log(jugador);
+// console.log(jugador);
