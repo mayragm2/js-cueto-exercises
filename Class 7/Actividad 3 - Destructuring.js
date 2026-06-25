b8c8309e52f0c568a965b0dc3f4c4bcb520b7c25
mayragm2 <mayracueto@gm2dev.com>
2026-06-25 12:49:50 -0300

    feat(activity 3): create new variable from object and display it

--- Class 7/Actividad 3 - Destructuring.js
+++ Class 7/Actividad 3 - Destructuring.js
@@ -9,3 +9,7 @@ const podio = ["Argentina", "Francia", "Croacia"];
 //Part A
 const {nombre, goles} = jugador;
 console.log (nombre, goles);
+
+//Part B
+const {nombre: nombreJugador} = jugador;
+console.log(nombreJugador);
