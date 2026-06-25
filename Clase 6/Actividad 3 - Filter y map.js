92a4bb1aa05df42437e1622cb56d8f4d846ef4c2
mayragm2 <mayracueto@gm2dev.com>
2026-06-25 10:36:54 -0300

    feat(activity 3): print amount of players with more than 2 goals and all player names

---
+++ Clase 6/Actividad 3 - Filter y map.js
@@ -0,0 +1,17 @@
+const jugadores = [
+  { nombre: "Messi",       goles: 8,  pais: "Argentina"  },
+  { nombre: "Mbappé",      goles: 5,  pais: "Francia"    },
+  { nombre: "Haaland",     goles: 12, pais: "Noruega"    },
+  { nombre: "Benzema",     goles: 0,  pais: "Francia"    },
+  { nombre: "Lewandowski", goles: 3,  pais: "Polonia"    },
+  { nombre: "Kane",        goles: 2,  pais: "Inglaterra" },
+  { nombre: "Neymar",      goles: 1,  pais: "Brasil"     },
+  { nombre: "Müller",      goles: 0,  pais: "Alemania"   },
+];
+
+const jugadores2goles = jugadores.filter (j => j.goles > 2);
+console.log (jugadores2goles.length); //print out the amount of players that have more than 2 goals
+
+const jugadoresNombres = jugadores.map (j => j.nombre);
+console.log (jugadoresNombres);
+
