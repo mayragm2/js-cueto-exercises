0aa54837a7ea1d87d24c9c03391cf918205b7e4c
mayragm2 <mayracueto@gm2dev.com>
2026-06-26 11:18:52 -0300

    feat(activity 5):add promedio key to jugador objects and calculate it

--- Class 7/Actividad 4 - Actualizar con spread.js
+++ Class 7/Actividad 4 - Actualizar con spread.js
@@ -1,15 +1,15 @@
-const jugador = {
-  nombre: "Mbappé",
-  goles: 5,
-  pais: "Francia",
-};
-const extra = { club: "Real Madrid", camiseta: 9};
+// const jugador = {
+//   nombre: "Mbappé",
+//   goles: 5,
+//   pais: "Francia",
+// };
+// const extra = { club: "Real Madrid", camiseta: 9};
 
-// Part A
-const jugadorActualizado = {...jugador, goles: 10};
-console.log(jugadorActualizado);
-console.log(jugador);
+// // Part A
+// const jugadorActualizado = {...jugador, goles: 10};
+// console.log(jugadorActualizado);
+// console.log(jugador);
 
-// Part B
-const jugadorCompleto = {...jugador, ...extra}
-console.log(jugadorCompleto);   
+// // Part B
+// const jugadorCompleto = {...jugador, ...extra}
+// console.log(jugadorCompleto);   
