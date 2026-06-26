ee7e208753ccbb284b948b49fb8a4968bb8874b3
mayragm2 <mayracueto@gm2dev.com>
2026-06-26 10:43:58 -0300

    feat(activity 4): merge player data objects and print via console

--- Class 7/Actividad 3 - Destructuring.js
+++ Class 7/Actividad 3 - Destructuring.js
@@ -1,27 +1,27 @@
-const jugador = {
-  nombre: "Haaland",
-  pais: "Noruega",
-  goles: 12,
-};
+// const jugador = {
+//   nombre: "Haaland",
+//   pais: "Noruega",
+//   goles: 12,
+// };
 
-const podio = ["Argentina", "Francia", "Croacia"];
+// const podio = ["Argentina", "Francia", "Croacia"];
 
-//Part A
-const {nombre, goles} = jugador;
-console.log (nombre, goles);
+// //Part A
+// const {nombre, goles} = jugador;
+// console.log (nombre, goles);
 
-//Part B
-const {nombre: nombreJugador} = jugador;
-console.log(nombreJugador);
+// //Part B
+// const {nombre: nombreJugador} = jugador;
+// console.log(nombreJugador);
 
-//Part C
-jugador.camiseta;   //Add camiseta 
-const {camiseta = 9} = jugador;
-console.log (camiseta);
+// //Part C
+// jugador.camiseta;   //Add camiseta 
+// const {camiseta = 9} = jugador;
+// console.log (camiseta);
 
-//Part D
-// const {[0]:oro, [1]:plata, [2]:bronce} = podio;
-const [oro, plata, bronce] = podio;
-console.log(oro);
-console.log(plata);
-console.log(bronce);
+// //Part D
+// // const {[0]:oro, [1]:plata, [2]:bronce} = podio;
+// const [oro, plata, bronce] = podio;
+// console.log(oro);
+// console.log(plata);
+// console.log(bronce);
