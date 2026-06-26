ff7bfc041199474a2ecbcce694ffd7cf180b45db
mayragm2 <mayracueto@gm2dev.com>
2026-06-26 10:34:05 -0300

    feat(activity 1): destructure podium object and print its variables

--- Class 7/Actividad 3 - Destructuring.js
+++ Class 7/Actividad 3 - Destructuring.js
@@ -15,5 +15,13 @@ const {nombre: nombreJugador} = jugador;
 console.log(nombreJugador);
 
 //Part C
-jugador.camiseta = 10;
-console.log (jugador);
+jugador.camiseta;   //Add camiseta 
+const {camiseta = 9} = jugador;
+console.log (camiseta);
+
+//Part D
+// const {[0]:oro, [1]:plata, [2]:bronce} = podio;
+const [oro, plata, bronce] = podio;
+console.log(oro);
+console.log(plata);
+console.log(bronce);
