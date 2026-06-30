49d4acb2b6e5530421fd5c4c139a7beec76267fc
mayragm2 <mayracueto@gm2dev.com>
2026-06-30 12:34:49 -0300

    feat(A5): add subtotal calculation

--- Clase 3/Actividad 5 - Precio con descuento de socio.js
+++ Clase 3/Actividad 5 - Precio con descuento de socio.js
@@ -1,1 +1,13 @@
+const precioUnitario = 1500;   // precio de una entrada, en pesos
+const cantidad = 5;            // cuántas entradas compra
+const esSocio = true;          // ¿es socio del club?
+let subtotal = precioUnitario * cantidad;
 
+if(cantidad>=4){
+    subtotal = subtotal * 0.9;
+}
+if (esSocio === true){
+    subtotal = subtotal * 0.85;
+}
+
+console.log (`Total a Pagar: $${subtotal}`);
